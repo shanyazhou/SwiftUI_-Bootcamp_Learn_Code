60b9aa4b32f0d6179ac722887adcd9f245d46ffe
shanyazhou <shan.yazhou@youyisheng.com>
2024-07-31 17:09:04 +0800

    add: 代码集合到文件夹内

---
+++ SwiftUI_Bootcamp_Learn_Code/SwiftUI_Bootcamp_Learn_Code/HomeView.swift
@@ -0,0 +1,52 @@
+//
+//  HomeView.swift
+//  SwiftUI_Bootcamp_Learn_Code
+//
+//  Created by SYZ on 2024/7/31.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack{
+            List{
+                Section {
+                    NavigationLink("HomeListView") {
+                        HomeListView()
+                    }
+                } header: {
+                    Text("SwiftUIBootcamp")
+                        .foregroundStyle(.red)
+                }
+                
+                
+                Section {
+                    NavigationLink("SwiftUI Todo List") {
+                        Text("SwiftUI Todo List")
+                    }
+                } header: {
+                    Text("SwiftUI Todo List")
+                        .foregroundStyle(.yellow)
+                }
+                
+                Section {
+                    NavigationLink("SwiftUI Continued Learning") {
+                        HomeListView()
+                    }
+                } header: {
+                    Text("SwiftUI Continued Learning")
+                        .foregroundStyle(.green)
+                }
+            }
+            
+           
+            
+            .navigationTitle("Main Entrance")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
